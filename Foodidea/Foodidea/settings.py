1706dedb77ccebe08400879161fb72b514b69ce1
lololepetitrigo <froufroutouch@gmail.com>
2021-04-10 17:28:39 +0200

    try2

--- Foodidea/Foodidea/settings.py
+++ Foodidea/Foodidea/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from os import path as oSpath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'd$!drh&qsk@owcl+isl6$f_y@@x3*d=gvb_d8%z%-ic0rq4*^$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = oSpath.join(BASE_DIR, 'static')
